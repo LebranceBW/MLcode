a57a32a861515b6816480d87e09dd6c8266adc41
Lebranc <qinghuabw@163.com>
2017-12-29 17:19:51 +0800

    🔨实现了BP神经网络,Tensorflow有点难用

--- NeuralNet/BPNN.py
+++ NeuralNet/BPNN.py
@@ -1,46 +1,69 @@
-#encoding:utf-8
+# encoding:utf-8
+'''
+    🕸 累积BP与标准BP
+'''
 import tensorflow as tf
 import sys
-from os import path 
+import time
 import numpy as np
+from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from dataSet.watermelon_3 import wm_dataset,wm_trainningset,wm_validationset
+from dataSet.watermelon_3 import wm_trainningset, wm_validationset
 
-#添加隐藏层
-def addlayer(x,insize,outsize,activate_func = None,name = "layer"):
-    with tf.name_scope('layer'):
-        with tf.name_scope('weights'):
-            w = tf.Variable(tf.random_normal([insize,outsize]),name=name+'_w')
-        with tf.name_scope('bias'):
-            b = tf.Variable(tf.zeros([1,outsize]),name=name+'_b')
-        x_mul_w_plus_b = tf.matmul(x,w) + b
-        if activate_func == None:
-            return x_mul_w_plus_b
-        else: 
-            return activate_func(x_mul_w_plus_b)
-#1 定义变量
-x = tf.placeholder("float",[None,8])
-y_ = tf.placeholder("float",[None,1])
-#2 定义层
-hiddenlayer = addlayer(x,8,12,tf.nn.sigmoid,"hiddenlayer")
-y = addlayer(hiddenlayer,12,1,tf.nn.sigmoid,"outputlayer")
-#3 定义评价函数与optimizer
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(y - y_),axis=[1]))
-with tf.name_scope('summaries1'):    
-    tf.summary.scalar("loss:",loss)
-train_step = tf.train.GradientDescentOptimizer(0.4).minimize(loss)
+LearningRate = 0.05
+hiddenlayerorder = 8
 
-merged = tf.summary.merge_all()
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    test_writer = tf.summary.FileWriter(r"G:\VSCodeWorkspace\ML_coding\NeuralNet\logs",sess.graph)
-    for i in range(2000):
-        sess.run(train_step,feed_dict={x:wm_trainningset[0],y_:wm_trainningset[1]})
-        result = sess.run(merged,feed_dict={x:wm_trainningset[0],y_:wm_trainningset[1]})
-        test_writer.add_summary(result,i) #result是summary类型的，需要放入writer中，i步数（x轴）
-        if i%100 == 0:
-            print("第%d次训练集误差:%.6f" % (i,sess.run(loss,feed_dict={x:wm_trainningset[0],y_:wm_trainningset[1]})))
-    print("训练完成后各个网络参数为")
-    for var in tf.global_variables():
-        print("%s \n %s" % (var.name,sess.run(var,feed_dict={x:wm_trainningset[0],y_:wm_trainningset[1]})))
-        
+
+def addlayer(prelayer, inputshape, outputshape, activate_func=None, name="layer"):
+    '''
+        添加中间层
+    '''
+    weighs = tf.Variable(tf.random_normal([inputshape, outputshape]), name=name + '_w')
+    bias = tf.Variable(tf.zeros([1, outputshape]), name=name + '_b')
+    x_mul_w_plus_b = tf.matmul(prelayer, weighs) + bias
+    if activate_func is None:   #Python中的对象包含三要素：id、type、value。is判断通过id来判断的。==通过value来判断的。
+        return x_mul_w_plus_b
+    return activate_func(x_mul_w_plus_b)
+
+
+def main():
+    '''
+        主函数
+    '''
+    # 1 定义变量
+    feature_space = tf.placeholder("float", [None, 8])
+    label_space = tf.placeholder("float", [None, 1])
+    # 2 定义层
+    hiddenlayer = addlayer(feature_space, 8, hiddenlayerorder, tf.nn.sigmoid, "hiddenlayer")
+    prediction = addlayer(hiddenlayer, hiddenlayerorder, 1, tf.nn.sigmoid, "outputlayer")
+    # 3 定义评价函数与optimizer
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(prediction - label_space), axis=[1]))
+    # 4 训练集上的准确率
+    error_rate = tf.reduce_mean(tf.abs(tf.cast(tf.greater(prediction,0.5),"float")-label_space))
+    with tf.name_scope('summaries1'):
+        tf.summary.scalar("loss:", loss)
+    train_step = tf.train.GradientDescentOptimizer(LearningRate).minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        i,minval = 0,1
+        while True:
+            sess.run(train_step,feed_dict={feature_space:wm_trainningset[0],label_space:wm_trainningset[1]})
+            cur = sess.run(loss,feed_dict={feature_space:wm_validationset[0],label_space:wm_validationset[1]})
+            if i%100 == 0:
+                print("第%d次测试集误差:%.6f" % (i,cur))
+            if cur < minval:
+                minval = cur
+                accuracy = 1 - sess.run(error_rate,feed_dict={feature_space:wm_validationset[0],label_space:wm_validationset[1]})
+                count = i
+            elif (cur - minval)> 0.1:
+                print("累积训练最终误差%f" % minval)
+                break
+            i,last = i+1,cur
+        with open("./BPNN.log",mode="a") as f:
+            f.writelines(time.strftime('%m-%d %Hu:%M',time.localtime(time.time()))+"\n")
+            f.writelines(u"  学习率为{learningrate},隐含层维度为{order},最终测试集误差为{loss:.5f},训练了{count}次,正确率为:{accu:.5f}\n"\
+            .format(learningrate = LearningRate,order = hiddenlayerorder,loss = minval,count = count,accu = accuracy))
+
+if __name__ == '__main__':
+    main()
